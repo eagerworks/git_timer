de47708eed753f1ca5f014187bcf3377ed7535a8
Guillermo Aguirre <guillermoaguirre1@gmail.com>
2018-06-28 14:57:31 -0300

    Add functionality to work command. Fix main file override when main was run and file already existed

--- lib/git_timer/cli.rb
+++ lib/git_timer/cli.rb
@@ -5,16 +5,22 @@ module GitTimer
     desc "work ACTION", "This will start or stop your work tracking"
     long_desc <<-WORK
 
-    `work start` will insert 'Work started | 2018-01-01 00:00:00 -0300' in the git log
+    `work start` will insert 'Work | Start | 2018-01-01 00:00:00 -0300' in the user-log file
 
-    `work stop` will insert 'Work stopped | 2018-01-01 00:00:00 -0300' in the git log
+    `work stop` will insert 'Work | Stop | 2018-01-01 00:00:00 -0300' in the user-log file
 
     With the timestamp being the current time with the format %yyyy-MM-dd hh:mm:ss time_zone
     WORK
-    option :upcase
+
     def work(action)
-      "#{action}".upcase! if options[:upcase]
-      puts action
+      case action
+        when 'start', 'START', 'Start'
+          GitTimer.register_activity({ ticket_id: 'Work', ticket_state: 'Start' })
+        when 'stop', 'STOP', 'Stop'
+          GitTimer.register_activity({ ticket_id: 'Work', ticket_state: 'Stop' })
+        else
+          puts 'Invalid action, please use `git_timer start` or `git_timer stop`. Type `git_timer help` for more information.'
+      end
     end
   end
 end
